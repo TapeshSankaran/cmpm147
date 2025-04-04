7a24b2a6a2d0b85912f20fb6fd2ef3c6f19f0e84
TapeshSankaran <Tapitina90@gmail.com>
2025-04-04 10:03:30 -0700

    Glitch transfer

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,49 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+const fillers = {
+  hero: ["Hero", "Soldier", "Citizen", "Fellow", "Champion", "$adventurer and $adventurer", "Chosen One"],
+  pre: ["council", "house", "delegation", "corporation", "gathering", "conclave"],
+  post: ["wizards", "ninjas", "lords","overseers", "appraisers", "old men", "CEO's"],
+  help: ["protect", "aid", "assist", "safeguard", "shield", "preserve"],
+  place: ["realms", "cities", "United States", "world", "land of liberty", "wallets", "countries", "people"],
+  looty: ["gleaming", "valuable", "esteemed", "rare", "exalted", "scintillating", "kinda gross but still usefull", "complete garbage"],
+  loots: ["power", "ability", "potential", "powers", "forces"],
+  baddies: ["7 Deadly Sins", "$pre of destruction", "Hellish Bunch", "Evil One", "Cult of Antipowers", "Agents of Chaos", "Sudominian Mafia"],
+  powery: ["Radiant", "Abyssal", "Vengeful", "Ethereal", "Majestic", "Merciless", "Eternal", "Savage", "Celestial", "Luminous", "Unyielding", "Fiery", "Frostbitten", "Ancient", "Cursed", "Lethal", "Divine", "Sinister", "Mystical", "Chaotic"],
+  powers: ["Time Manipulation", "Invisibility", "Telekinesis", "Shapeshifting", "Elemental Control", "Healing Touch", "Teleportation", "Mind Reading", "Energy Absorption", "Super Strength", "Flight", "Illusion Creation", "Dimensional Travel", "Weather Manipulation", "Technomancy", "Gravity Control", "Sound Manipulation", "Reality Warping", "Summoning", "Foresight", "Chronoportation", 
+           "Subatomic Manipulation", "Chronoception", "Bio-mimicry", "Tactile Teleportation", "Psychic Fabrication", "Dreamwalking", "Biokinesis", "Voidwalking", "Transmutation", "Memory Manipulation", "Precognition", "Soundscaping", "Aura Reading","Shadowmancy", "Quantum Tunneling", "Dreamweaving", "Linguistic Enchantment", "Lightbending", "Molecular Densification"]
+};
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
+const template = `Hello $hero,
+I am the last of the $pre of $post, sworn to $help the $place from the $baddies. 
+But I have grown weak and seek a $hero to inherit my $looty $loots. My $loots are $powery $powers.
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
+Do you accept?
+`;
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
+
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  box.innerText = story;
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+clicker.onclick = generate;
+
+generate();
