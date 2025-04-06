6311c032fb04abdb73eb75cdb3e997c01315890d
TapeshSankaran <Tapitina90@gmail.com>
2025-04-06 10:17:28 -0700

    More space created

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -7,18 +7,18 @@ const fillers = {
   place: ["realms", "cities", "United States", "world", "land of liberty", "wallets", "countries", "people"],
   looty: ["gleaming", "valuable", "esteemed", "rare", "exalted", "scintillating", "kinda gross but still useful", "complete garbage", "wacky", "supreme", "special", "indomitable", "$looty, $looty"],
   loots: ["power", "ability", "potential", "powers", "forces"],
-  loots2: ["power", "ability", "potential"],
+  loots2: ["power", "ability", "talent", "skill", "affinity"],
   baddies: ["7 Deadly Sins", "$pre of destruction", "Hellish Bunch", "Evil One", "Cult of Antipowers", "Agents of Chaos", "Sudominian Mafia"],
   powery: ["Radiant", "Abyssal", "Vengeful", "Ethereal", "Majestic", "Merciless", "Eternal", "Savage", "Celestial", "Luminous", "Unyielding", "Fiery", "Frostbitten", "Ancient", "Cursed", "Lethal", "Divine", "Sinister", "Mystical", "Chaotic"],
   powers: ["Time Manipulation", "Invisibility", "Telekinesis", "Shapeshifting", "Elemental Control", "Healing Touch", "Teleportation", "Mind Reading", "Energy Absorption", "Super Strength", "Flight", "Illusion Creation", "Dimensional Travel", "Weather Manipulation", "Technomancy", "Gravity Control", "Sound Manipulation", "Reality Warping", "Summoning", "Foresight", "Chronoportation", 
            "Subatomic Manipulation", "Chronoception", "Bio-mimicry", "Tactile Teleportation", "Psychic Fabrication", "Dreamwalking", "Biokinesis", "Voidwalking", "Transmutation", "Memory Manipulation", "Precognition", "Soundscaping", "Aura Reading","Shadowmancy", "Quantum Tunneling", "Dreamweaving", "Linguistic Enchantment", "Lightbending", "Molecular Densification"]
 };
 
-const template = `Hello $hero,\n
+const template = `\n\nHello $hero,\n
 I am the last of the $pre of $post, sworn to $help the $place from the $baddies.\n
 But I have grown weak and seek a $hero2 to inherit my $looty $loots. My $loots2 is $powery $powers.\n\n
 
-Do you accept?
+Do you accept?\n\n
 `;
 
 // STUDENTS: You don't need to edit code below this line.
