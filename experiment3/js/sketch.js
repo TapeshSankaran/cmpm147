c07b05c9f305657dfeff85d5ad56249fff2eb950
TapeshSankaran <49103180+TapeshSankaran@users.noreply.github.com>
2025-04-21 01:29:42 -0700

    First world in

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -15,6 +15,145 @@ let myInstance;
 let canvasContainer;
 var centerHorz, centerVert;
 
+  /* exported preload, setup, draw, placeTile */
+
+/* global generateGrid drawGrid */
+
+let seed = 0;
+let tilesetImage;
+let currentGrid = [];
+let numRows, numCols;
+
+function preload() {
+  tilesetImage = loadImage(
+    "https://cdn.glitch.com/25101045-29e2-407a-894c-e0243cd8c7c6%2FtilesetP8.png?v=1611654020438"
+  );
+}
+
+function reseed() {
+  seed = (seed | 0) + 1109;
+  randomSeed(seed);
+  noiseSeed(seed);
+  select("#seedReport").html("seed " + seed);
+  regenerateGrid();
+}
+
+function regenerateGrid() {
+  select("#asciiBox").value(gridToString(generateGrid(numCols, numRows)));
+  reparseGrid();
+}
+
+function reparseGrid() {
+  currentGrid = stringToGrid(select("#asciiBox").value());
+}
+
+function gridToString(grid) {
+  let rows = [];
+  for (let i = 0; i < grid.length; i++) {
+    rows.push(grid[i].join(""));
+  }
+  return rows.join("\n");
+}
+
+function stringToGrid(str) {
+  let grid = [];
+  let lines = str.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    let row = [];
+    let chars = lines[i].split("");
+    for (let j = 0; j < chars.length; j++) {
+      row.push(chars[j]);
+    }
+    grid.push(row);
+  }
+  return grid;
+}
+
+function placeTile(i, j, ti, tj) {
+  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
+}
+
+/* exported generateGrid, drawGrid */
+/* global placeTile */
+
+const lookup = [
+  [0, -1], [10, 0], [10, 2], [10, 0],
+  [11, 1], [11, 0], [11, 2], [11, 0],
+  [9, 1], [9, 0], [9, 2], [9, 0],
+  [11, 1], [10, 0], [10, 2], [10, 0]
+];
+
+
+function generateGrid(numCols, numRows) {
+  let grid = [];
+  let scale = 0.1;
+  for (let i = 0; i < numRows; i++) {
+    let row = [];
+    for (let j = 0; j < numCols; j++) {
+      if (noise(i * scale, j * scale) > 0.5) {
+        row.push('.');
+      } else {
+        row.push('_');
+      }
+    }
+    grid.push(row);
+  }
+  
+  return grid;
+}
+
+function drawGrid(grid) {
+  noStroke();
+  fill('#ffffff75')
+  background(128);
+
+  for(let i = 0; i < grid.length; i++) {
+    for(let j = 0; j < grid[i].length; j++) {
+      if (gridCheck(grid, i, j, '_')) {
+        placeTile(i, j, random(4) | 0, 0);
+        if (random() < 0.01) {
+          placeTile(i, j, 26, random(4)|0);
+        } else if (random() < 0.05) {
+          placeTile(i, j, 14, 6);
+        } 
+      } else {
+        placeTile(i, j, random(4)|0, 13);
+        drawContext(grid, i, j, '_', 0, 0);
+      }
+    }
+
+  }
+  for (let i = 0; i < 10; i++) {
+    let z = random(40)
+    
+    ellipse(500*(random()+(millis()/(z*6000)))%500-50, random(650), 40+z, 10+z)
+  }
+}
+
+function gridCheck(grid, i, j, target) {
+  if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
+    return false;
+  }
+  return grid[i][j] === target;
+}
+
+
+function gridCode(grid, i, j, target) {
+  const northBit = gridCheck(grid, i - 1, j, target) ? 1 : 0;
+  const southBit = gridCheck(grid, i + 1, j, target) ? 1 : 0;
+  const eastBit  = gridCheck(grid, i, j + 1, target) ? 1 : 0;
+  const westBit  = gridCheck(grid, i, j - 1, target) ? 1 : 0;
+
+  return (northBit << 0) + (southBit << 1) + (eastBit << 2) + (westBit << 3);
+}
+
+
+function drawContext(grid, i, j, target, ti, tj) {
+  const code = gridCode(grid, i, j, target);
+  const [tiOffset, tjOffset] = lookup[code];
+  placeTile(i, j, ti + tiOffset, tj + tjOffset);
+}
+
 class MyClass {
     constructor(param1, param2) {
         this.property1 = param1;
@@ -31,46 +170,33 @@ function resizeScreen() {
   centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
   console.log("Resizing...");
   resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
+  //redrawCanvas(); // Redraw everything based on new size
 }
 
 // setup() function is called once when the program starts
 function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
+  //canvasContainer = $("#canvas-container")
+  numCols = select("#asciiBox").attribute("cols") | 0;
+  numRows = select("#asciiBox").attribute("rows") | 0;
+  console.log(numCols);
+  createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer");
+  select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
 
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
+  select("#reseedButton").mousePressed(reseed);
+  select("#asciiBox").input(reparseGrid);
+/*
   $(window).resize(function() {
     resizeScreen();
   });
   resizeScreen();
+*/
+  reseed();
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  randomSeed(seed);
+  drawGrid(currentGrid);
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
